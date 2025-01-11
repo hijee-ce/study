2affa8a72c5760d08568cb80ac52874a5bf5d8d3
Jihee <wlgml6925@gmail.com>
2025-01-11 22:38:41 +0900

    [250111] : 프로그래머스 - 카펫 크기

---
+++ Algorithm/Programmers/Python/카펫크기.py
@@ -0,0 +1,14 @@
+# 카펫 크기
+
+def solution(brown, yellow):
+    i = 1
+    while True:
+        y_h = i
+        y_w = yellow / y_h
+
+        if brown == (y_h * 2) + ((y_w + 2) * 2):
+            answer = [round(y_w + 2), (y_h + 2)]
+            break
+        i += 1
+
+    return answer
